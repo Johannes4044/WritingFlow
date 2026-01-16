cee0733b7b72b0c4c5dc1de0983fe6a201a9dfb6
Johannes <dau.johannes@outlook.com>
2026-01-16 09:02:44 +0100

    Initial Commit

---
+++ WritingFlowTests/WritingFlowTests.swift
@@ -0,0 +1,16 @@
+//
+//  WritingFlowTests.swift
+//  WritingFlowTests
+//
+//  Created by Johannes on 16.01.26.
+//
+
+import Testing
+
+struct WritingFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
